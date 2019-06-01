3d6bd34e1a8ad731f00f05f847941b82c6a0756c
matheusor99 <matheusor99@gmail.com>
2019-06-01 20:41:56 -0300

    criando elementos e transformando-os em filhos de outros elementos alem de addEventListener e preventDefault

--- JavaScript/introducao-javascript/js/principal.js
+++ JavaScript/introducao-javascript/js/principal.js
@@ -31,3 +31,36 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = "Altura e/ou peso inválidos!"
     }
 }
+
+
+var botao = document.querySelector("#adicionar-paciente");
+botao.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#dados-paciente");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);    
+
+    var tbody = document.querySelector("#tabela-pacientes");
+
+    tbody.appendChild(pacienteTr)
+})
