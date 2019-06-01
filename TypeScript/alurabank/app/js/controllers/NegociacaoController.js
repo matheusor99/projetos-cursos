8a54e5a9b43b10b1bd0c96e1f30b0ffe819b7e4c
matheusor99 <matheusor99@gmail.com>
2019-06-01 14:59:29 -0300

    compilador ts para js no tsconfig.json e package.json

---
+++ TypeScript/alurabank/app/js/controllers/NegociacaoController.js
@@ -0,0 +1,10 @@
+class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona(evento) {
+        alert("minha logica aqui");
+    }
+}
