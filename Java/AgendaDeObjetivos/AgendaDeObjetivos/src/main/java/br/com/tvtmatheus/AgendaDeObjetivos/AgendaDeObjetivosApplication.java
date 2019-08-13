a0602cb837a601af67bbfd6b43c87d84a6ad6023
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-08-12 21:22:21 -0300

    projeto da faculdade

---
+++ Java/AgendaDeObjetivos/AgendaDeObjetivos/src/main/java/br/com/tvtmatheus/AgendaDeObjetivos/AgendaDeObjetivosApplication.java
@@ -0,0 +1,12 @@
+package br.com.tvtmatheus.AgendaDeObjetivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeObjetivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeObjetivosApplication.class, args);
+	}
+}
