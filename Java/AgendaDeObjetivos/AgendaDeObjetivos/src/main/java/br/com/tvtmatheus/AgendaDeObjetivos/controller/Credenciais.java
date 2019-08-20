2c46118ebaf51b12219d6daab5eb522c5055468d
matheusor99 <matheusor99@gmail.com>
2019-08-20 14:15:40 -0300

    classe X factory

--- Java/AgendaDeObjetivos/AgendaDeObjetivos/src/main/java/br/com/tvtmatheus/AgendaDeObjetivos/controller/Credenciais.java
+++
@@ -1,9 +0,0 @@
-package br.com.tvtmatheus.AgendaDeObjetivos.controller;
-
-public class Credenciais {
-
-	public Credenciais(String string, String string2) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
